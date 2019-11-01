48cde1a78f8794f25e3417c1593fee9ce25481c3
Frank Shedleski <fshedleski@gmail.com>
2019-11-01 14:06:10 -0400

    roguelike2 submitted version

--- roguelike2/src/game/PlayerLComponent.java
+++ roguelike2/src/game/PlayerLComponent.java
@@ -10,8 +10,6 @@ public class PlayerLComponent extends Component {
     public Direction dir;
     public int MAXX, MAXY;
     public final int SPEED = 4;
-    private int shootDelay = 0;
-    private boolean shootEye = false;
 
     public PlayerLComponent(GameObject _parent) {
         super(_parent);
@@ -40,34 +38,6 @@ public class PlayerLComponent extends Component {
                 this.dir = Direction.Right;
                 this.move();
             }
-            if (engine.EngineCore.inputs.pending.contains(KeyEvent.VK_SPACE) && shootDelay == 0) {
-                GameObject bullet;
-                if(dir == Direction.Left || dir == Direction.Right) {
-                    bullet = new GameObject(parent.corePtr, (int)parent.af.getTranslateX(), (int)parent.af.getTranslateY()+15);
-                } else {
-                    if(shootEye) {
-                        bullet = new GameObject(parent.corePtr, (int)parent.af.getTranslateX()+2, (int)parent.af.getTranslateY()+8);
-                    } else {
-                        bullet = new GameObject(parent.corePtr, (int)parent.af.getTranslateX()+30, (int)parent.af.getTranslateY()+8);
-                    }
-                    shootEye = !shootEye;
-                }
-                BulletLComponent bulletLComponent = new BulletLComponent(bullet, dir);
-                BulletGComponent bulletGComponent = new BulletGComponent(bullet, dir);
-                if(dir == Direction.Left || dir == Direction.Right || dir == Direction.Down) {
-                    bulletLComponent.Priority = 3;
-                    bulletGComponent.Priority = 3;
-                } else {
-                    bulletLComponent.Priority = 1;
-                    bulletGComponent.Priority = 1;
-                }
-                bullet.addLogicComponent(bulletLComponent);
-                bullet.addGraphicsComponent(bulletGComponent);
-                shootDelay = 15;
-            }
-        }
-        if(shootDelay > 0) {
-            shootDelay--;
         }
     }
 
@@ -78,28 +48,28 @@ public class PlayerLComponent extends Component {
         switch(_dir) {
             case Up:
                 if(parent.af.getTranslateY() - SPEED < 0) {
-                    parent.af.setToTranslation(parent.af.getTranslateX(), MAXY-1);
+                    //parent.af.setToTranslation(parent.af.getTranslateX(), MAXY-1);
                 } else {
                     parent.af.translate(0,-SPEED);
                 }
                 break;
             case Down:
                 if(parent.af.getTranslateY() + SPEED >= MAXY) {
-                    parent.af.setToTranslation(parent.af.getTranslateX(),0);
+                    //parent.af.setToTranslation(parent.af.getTranslateX(),0);
                 } else {
                     parent.af.translate(0,SPEED);
                 }
                 break;
             case Left:
                 if(parent.af.getTranslateX() - SPEED < 0) {
-                    parent.af.setToTranslation(MAXX-1, parent.af.getTranslateY());
+                    //parent.af.setToTranslation(MAXX-1, parent.af.getTranslateY());
                 } else {
                     parent.af.translate(-SPEED,0);
                 }
                 break;
             case Right:
                 if(parent.af.getTranslateX() + SPEED >= MAXX) {
-                    parent.af.setToTranslation(0, parent.af.getTranslateY());
+                    //parent.af.setToTranslation(0, parent.af.getTranslateY());
                 } else {
                     parent.af.translate(SPEED,0);
                 }
